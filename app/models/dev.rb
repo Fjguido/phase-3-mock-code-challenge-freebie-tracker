cc405d92428ccaca1fcd6cbdea05f995ff4deb28
Francis Guido <fjguido97@gmail.com>
2022-03-02 10:18:57 -0500

    commit tree ffe4303097b405eaa41b9c0e9cfd45311adaba39

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        !! self.freebie.find_by(item_name: item_name)
+    end
+
+    def give_away(dev, freebie)
+        self.freebies.update(dev_id: dev.id) unless freebie.dev != self
+        "not yours"
+    end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,24 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
+c3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+c4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
+d3 = Dev.create(name: "Mr. Meseeks")
+d4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+
+f1 = Freebie.create(item_name: "paper", value: 1, dev_id: d1.id, company_id: c1.id )
+f2 = Freebie.create(item_name: "coffee", value: 2, dev_id: d2.id, company_id: c3.id )
+f3 = Freebie.create(item_name: "pen", value: 3, dev_id: d3.id, company_id: c1.id )
+f4 = Freebie.create(item_name: "pepper", value: 4, dev_id: d4.id, company_id: c3.id )
+f5 = Freebie.create(item_name: "coke", value: 5, dev_id: d2.id, company_id: c2.id )
+f6 = Freebie.create(item_name: "tire", value: 6, dev_id: d3.id, company_id: c2.id )
+f7 = Freebie.create(item_name: "wheel", value: 7, dev_id: d1.id, company_id: c1.id )
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
